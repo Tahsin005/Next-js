3ca5e81dd81b11c4d4e27d9ecd1fb6d94ac05403
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-16 20:54:58 +0600

    routing, nested routing, dynamic routes, nested dynamic routes in next.js

---
+++ 02_routing_demo/routing-demo/src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,8 @@
+export default async function ReviewList({
+    params,
+}: {
+    params: Promise<{ productId: string }>
+}) {
+    const productId = (await params).productId
+    return <h1>Reviews for Product {productId}</h1>
+}
