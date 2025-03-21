463812d56b6ddb19a4c3e487076199961ec055a2
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-22 02:32:43 +0600

    params and search params in next.js

--- 02_routing_demo/routing-demo/src/app/page.tsx
+++ 02_routing_demo/routing-demo/src/app/page.tsx
@@ -6,5 +6,9 @@ export default function Home() {
         <Link className="underline" href="/blog">blog</Link>
         <br />
         <Link className="underline" href="/products">products</Link>
+        <br />
+        <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+        <br />
+        <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
     </>
 }
