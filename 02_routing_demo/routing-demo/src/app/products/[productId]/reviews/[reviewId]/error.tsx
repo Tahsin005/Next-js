5c3ac8859ca9483439da94af421310741f3bb78e
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-22 12:28:33 +0600

    recovering from errors in next.js

--- 02_routing_demo/routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ 02_routing_demo/routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,25 @@
 "use client";
 
+import { startTransition } from "react";
+import { useRouter } from "next/navigation";
+
 export default function ErrorBoundary({
-    error,
+    error, reset
 }: {
-    error: Error
+    error: Error,
+    reset: () => void
 }) {
-    return <div>{error.message}</div>
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
+    return (
+        <>
+            <div>{error.message}</div>
+            <button onClick={() => reload()}>Try again</button>
+        </>
+    )
 }
