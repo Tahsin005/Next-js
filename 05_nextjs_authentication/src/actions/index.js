99e524efca8c035e385f790f624ef2f205ba3f87
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-02 03:05:02 +0600

    user login using server actions in next.js

--- 05_nextjs_authentication/src/actions/index.js
+++ 05_nextjs_authentication/src/actions/index.js
@@ -3,6 +3,8 @@
 import connectDB from "@/database";
 import User from "@/models";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function registerUserAction(formData) {
     await connectDB();
@@ -47,5 +49,54 @@ export async function registerUserAction(formData) {
             success: false,
             message: "Some error occurred! Please try again",
         };
+    }
+}
+
+
+export async function loginUserAction(formData) {
+    await connectDB();
+
+    try {
+        const { email, password } = formData;
+
+        const checkUser = await User.findOne({ email });
+        if (!checkUser) {
+            return {
+                success: false,
+                message: "User doesnot exist ! please sign up",
+            };
+        }
+
+        const checkPassword = await bcryptjs.compare(password, checkUser.password);
+        if (!checkPassword) {
+            return {
+                message: "Password is incorrect please check",
+                success: false,
+            };
+        }
+
+        const createdTokenData = {
+            id: checkUser._id,
+            userName: checkUser.userName,
+            email: checkUser.email,
+        };
+
+        const token = jwt.sign(createdTokenData, 'SUIII', {
+            expiresIn: "1d",
+        });
+
+        const getCookies = cookies();
+        getCookies.set("token", token);
+
+        return {
+            success: true,
+            message: "User logged in successfully",
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            success: false,
+            message: "Some error occurred! Please try again",
+        }
     }
 }
--- 05_nextjs_authentication/src/app/sign-in/page.js
+++ 05_nextjs_authentication/src/app/sign-in/page.js
@@ -1,11 +1,58 @@
 'use client';
 
-const SignIn = () => {
-    return (
-        <div>
-            <h1>Please Login</h1>
-        </div>
-    )
-}
+import CommonFormElement from "@/components/form-element/page";
+import { initialLoginFormData, userLoginFormControls } from "../utils";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { loginUserAction } from "@/actions";
+import toast from "react-hot-toast";
 
-export default SignIn
+const SignIn = () => {
+    const [signInFormData, setSignInFormData] = useState(initialLoginFormData);
+    const router = useRouter();
+
+    async function handleSignIn(event) {
+        event.preventDefault();
+
+        const result = await loginUserAction(signInFormData);
+        console.log(result);
+        if (result?.success) {
+            toast.success(result.message);
+            router.push("/");
+        } else {
+            toast.error(result.message);
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+                <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
+                <form onSubmit={handleSignIn} className="space-y-4">
+                    {userLoginFormControls.map((controlItem) => (
+                        <div key={controlItem.name}>
+                            <Label className="text-gray-700 mb-3">{controlItem.label}</Label>
+                            <CommonFormElement
+                                currentItem={controlItem}
+                                value={signInFormData[controlItem.name]}
+                                onChange={(event) =>
+                                    setSignInFormData({
+                                        ...signInFormData,
+                                        [event.target.name]: event.target.value,
+                                    })
+                                }
+                            />
+                        </div>
+                    ))}
+                    <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 text-white">
+                        Sign In
+                    </Button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
